1283983aa8a0a8e19e4a59c99f2e66ff1a5be578
Landon Key <>
2019-06-03 21:38:21 -0500

    Add project files.

---
+++ AwsLambdaDrop/Data/Data.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AwsLambdaDrop.Data
+{
+    public partial class AwsLinkingRequestObject
+    {
+        [JsonProperty("CallerIdentity")]
+        public CallerIdentity CallerIdentity { get; set; }
+
+        [JsonProperty("SecurityGroup")]
+        public SecurityGroup SecurityGroup { get; set; }
+
+        [JsonProperty("SecurityUser")]
+        public SecurityUser SecurityUser { get; set; }
+
+        [JsonProperty("AccessKey")]
+        public AwsLinkingRequestObjectAccessKey AccessKey { get; set; }
+    }
+
+    public partial class AwsLinkingRequestObjectAccessKey
+    {
+        [JsonProperty("AccessKey")]
+        public AccessKeyAccessKey AccessKey { get; set; }
+    }
+
+    public partial class AccessKeyAccessKey
+    {
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+        [JsonProperty("UserName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("AccessKeyId")]
+        public string AccessKeyId { get; set; }
+
+        [JsonProperty("CreateDate")]
+        public DateTimeOffset CreateDate { get; set; }
+
+        [JsonProperty("SecretAccessKey")]
+        public string SecretAccessKey { get; set; }
+    }
+
+    public partial class CallerIdentity
+    {
+        [JsonProperty("UserId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("Account")]
+        public string Account { get; set; }
+
+        [JsonProperty("Arn")]
+        public string Arn { get; set; }
+    }
+
+    public partial class SecurityGroup
+    {
+        [JsonProperty("Group")]
+        public Group Group { get; set; }
+    }
+
+    public partial class Group
+    {
+        [JsonProperty("Arn")]
+        public string Arn { get; set; }
+
+        [JsonProperty("GroupName")]
+        public string GroupName { get; set; }
+
+        [JsonProperty("CreateDate")]
+        public DateTimeOffset CreateDate { get; set; }
+
+        [JsonProperty("Path")]
+        public string Path { get; set; }
+
+        [JsonProperty("GroupId")]
+        public string GroupId { get; set; }
+    }
+
+    public partial class SecurityUser
+    {
+        [JsonProperty("User")]
+        public User User { get; set; }
+    }
+
+    public partial class User
+    {
+        [JsonProperty("UserId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("Arn")]
+        public string Arn { get; set; }
+
+        [JsonProperty("Path")]
+        public string Path { get; set; }
+
+        [JsonProperty("UserName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("CreateDate")]
+        public DateTimeOffset CreateDate { get; set; }
+    }
+
+    public partial class AwsLinkingRequestObject
+    {
+        public static AwsLinkingRequestObject FromJson(string json) => JsonConvert.DeserializeObject<AwsLinkingRequestObject>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this AwsLinkingRequestObject self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ AwsLambdaDrop/Function.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using AwsLambdaDrop.Data;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AwsLambdaDrop
+{
+    public class Function
+    {
+        public class jsonresponse
+        {
+            public bool isBase64Encoded { get; set; } = false;
+            public object headers { get; set; } = new object();
+            public int statusCode { get; set; } = 200;
+            public string body { get; set; }
+        }
+        private static string LINKED_ACCOUNTS = "sae.linked.accounts";
+
+        public async Task<jsonresponse> FunctionHandler(AwsLinkingRequestObject data, ILambdaContext context)
+        {
+            try
+            {
+                var s3Client = new AmazonS3Client(Environment.GetEnvironmentVariable("awsAccessKeyId"), Environment.GetEnvironmentVariable("awsSecretAccessKey"));
+                var fileTransferUtility = new TransferUtility(s3Client);
+                try
+                {
+                    await s3Client.GetObjectMetadataAsync(new Amazon.S3.Model.GetObjectMetadataRequest() { BucketName = LINKED_ACCOUNTS, Key = data.CallerIdentity.Account });
+                    return new jsonresponse { statusCode= 304, body = data.CallerIdentity.Account };
+                }
+                catch { }
+
+                using (var stream = new MemoryStream(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(data))))
+                {
+                    await fileTransferUtility.UploadAsync(stream, LINKED_ACCOUNTS, data.CallerIdentity.Account);
+                }
+
+                return new jsonresponse { body = data.CallerIdentity.Account };
+
+            }
+            catch (Exception ex)
+            {
+                if(Environment.GetEnvironmentVariable("awsDebug") == "true")
+                    return new jsonresponse { statusCode= 500, body = ex.ToString() };
+                else
+                    return new jsonresponse { statusCode = 500, body = "Something went wrong, set the environment variable awsDebug=true to see what it is." };
+            }
+        }
+    }
+}
